73692c8a380ee0e388d036a8754da51f39527acb
Schnoeff <02bburrell@live.co.uk>
2020-08-11 13:45:10 +0000

    Displayed package names correctly on to packages.html

--- packages/views.py
+++ packages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Package
 # Create your views here.
+
+
+def all_packages(request):
+    """ returns all of the packages available """
+
+    packages = Package.objects.all()
+
+    context = {
+        'packages': packages,
+    }
+
+    return render(request, 'packages/packages.html', context)
