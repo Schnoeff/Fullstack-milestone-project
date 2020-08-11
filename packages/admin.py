719a1a43cfc6a2a39f674786e31991f67fbcf069
Schnoeff <02bburrell@live.co.uk>
2020-08-11 12:42:34 +0000

    Customized admin screen

--- packages/admin.py
+++ packages/admin.py
@@ -4,5 +4,16 @@ from .models import Package, Category
 # Register your models here.
 
 
-admin.site.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'rating'
+    )
+
+
+ordering = ('sku',)
+
+admin.site.register(Package, PackageAdmin)
 admin.site.register(Category)
