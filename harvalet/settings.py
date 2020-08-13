db4c898a0349c1cc8c4ccd008db0356e46e86729
Schnoeff <02bburrell@live.co.uk>
2020-08-13 14:09:16 +0000

    Added shopping bag app and functionality

--- harvalet/settings.py
+++ harvalet/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'packages',
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
