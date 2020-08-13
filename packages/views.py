5822306015ab79c7dee656dabed0dd3f8c626b5a
Schnoeff <02bburrell@live.co.uk>
2020-08-13 12:53:18 +0000

    Added search bar queries resulting in a functioning search bar

--- packages/views.py
+++ packages/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Package
 # Create your views here.
 
@@ -7,9 +9,21 @@ def all_packages(request):
     """ returns all of the packages available """
 
     packages = Package.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No suitable search requirements met!")
+                return redirect(reverse('packages'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            packages = packages.filter(queries)
 
     context = {
         'packages': packages,
+        'search_term': query,
     }
 
     return render(request, 'packages/packages.html', context)
