1cff98b5160a30e6060db7ac06d06ab77891614c
Schnoeff <02bburrell@live.co.uk>
2020-08-28 00:19:54 +0000

    Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn harvalet.wsgi:application
--- harvalet/settings.py
+++ harvalet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'hg4v-*ry04d*dw2+^h(*4%68gmf4w3h*t^0%a7al#2py$i#c+f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['harvalet.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -115,12 +115,17 @@ WSGI_APPLICATION = 'harvalet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-     'ENGINE': 'django.db.backends.sqlite3',
-     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
- }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASE ={
+        'default' (dj_database_url).parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
